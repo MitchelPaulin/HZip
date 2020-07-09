9f7a673f551549cca7e017ddfdfe89a315756c1b
MitchelPaulin <mitchel0022@gmail.com>
2020-07-08 23:44:18 -0400

    Started to get someting that looks like an lz77 encoding working

--- lz77.hs
+++ lz77.hs
@@ -3,18 +3,32 @@ import System.IO
 import System.Environment
 import qualified Data.ByteString as B
 
+bufferSize = 10
+lookaheadSize = 4
+
+testString = "AABCBBABC"
+
 main :: IO ()
 main = do
     userInput <- getArgs
-    bytes <- B.readFile (head userInput)
-    putStrLn $ show bytes
+    bytes <- readFile (head userInput)
+    putStrLn $ show $ map (getLZ77Encoding bytes) [0..((length bytes - 1))]
 
+getLZ77Encoding :: Eq a => [a] -> Int -> (Maybe Int, [a])
+getLZ77Encoding byteString prefixIndex = (longestPrefix buffer lookahead)
+                                                where buffer = slice (max (prefixIndex - bufferSize) 0) prefixIndex byteString
+                                                      lookahead = slice prefixIndex (prefixIndex + lookaheadSize) byteString
 
--- the main idea of the longest prefix match is to look back in the buffer and find a reference to that string
+-- | The 'longestPrefix' function finds the longest prefix of 'prefix' that exists in the buffer, return Nothing if no such prefix exists
 longestPrefix :: Eq a => [a] -> [a] -> (Maybe Int, [a])
 longestPrefix _ [] = (Nothing, [])
-longestPrefix buffer prefix = if intersect prefix buffer == prefix then (findSubstring prefix buffer, prefix)
+longestPrefix _ [x] = (Nothing, [x])
+longestPrefix buffer prefix = if findSubstring prefix buffer /= Nothing then (findSubstring prefix buffer, prefix)
                                 else longestPrefix buffer (init prefix)
 
+-- | The 'findSubstring' function finds the index of the start of pat in str
 findSubstring :: Eq a => [a] -> [a] -> Maybe Int
 findSubstring pat str = findIndex (isPrefixOf pat) (tails str) 
+
+slice :: Int -> Int -> [a] -> [a]
+slice from to xs = take (to - from) (drop from xs)
